2bcf0256f45e7aa32a55bb3133627ef8ca184b53
dhq_boiler <dhq_boiler@live.jp>
2026-02-10 16:51:22 +0900

    feat: add ChatManageable controller concern
    Add a concern providing chat initialization, active chat tracking,
    and chat collection management for controllers via Engine initializer.
    
    # Conflicts:
    #	lib/chat_manager.rb

--- lib/chat_manager/engine.rb
+++ lib/chat_manager/engine.rb
@@ -1,5 +1,11 @@
 module ChatManager
   class Engine < ::Rails::Engine
     isolate_namespace ChatManager
+
+    initializer "chat_manager.controllers" do
+      ActiveSupport.on_load(:action_controller) do
+        include ChatManager::ChatManageable
+      end
+    end
   end
 end
